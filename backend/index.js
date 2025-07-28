9cd382a943f5e3671928233a679eed2d173ff3a1
rajeshn67 <rajeshnarwade67@gmail.com>
2025-07-28 22:33:17 +0530

    Code added

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const EmployeeRoutes = require('./Routes/EmployeeRoutes');
 const PORT = process.env.PORT || 8080;
 
 require('./Models/db');
-app.use(cors());
+app.use(cors({ origin: process.env.Fronend_URL || 'http://localhost:3000' }));
 app.use(bodyParser.json());
 
 app.use('/api/employees', EmployeeRoutes);
